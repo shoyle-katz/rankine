2dbf19a5723358a6a984f3add6cfe82d6f864c9d
Michael Selik <mike@selik.org>
2014-01-29 00:31:39 -0500

    break out line parsing into a function

--- grab-stations.py
+++ grab-stations.py
@@ -2,6 +2,7 @@
 import requests
 import argparse
 import sys
+import logging
 
 def get_command_line_args():
     script_desc = '''Get weather forecast station locations.
@@ -32,26 +33,28 @@ def extract_illinois_section(text):
     tag_end   = text.find('PRE', il_start)
     return text[tag_start+4:tag_end-2]
 
-if __name__=='__main__':
-    args = get_command_line_args()
-    text = grab_website_text()
-    data = extract_illinois_section(text)
-
-    #   Extract latitude and longitude of stations.  We know the columns are fixed
-    #   (which is both inconvenient and convenient).  In this case, we will simply
-    #   set the limits of the relevant columns by counting the number of columns
-    #   over we need to go.
+def parse_station_line(line):
+    '''Extract latitude and longitude of stations. We know the columns are fixed
+    (which is both inconvenient and convenient). In this case, we will simply
+    set the limits of the relevant columns by counting the number of columns
+    over we need to go.'''
     r_stn  = (5, 9) #remember that the last index is an exclusive bound
     r_name = (10, 31)
     r_lat  = (36, 41) #we don't need the N/W designation; we know where we are
     r_lon  = (46, 51)
-    for line in data.split('\n'):
-        stn  = line[r_stn[0]:r_stn[1]]
-        name = line[r_name[0]:r_name[1]]
+    stn  = line[r_stn[0]:r_stn[1]]
+    name = line[r_name[0]:r_name[1]]
+    lat  =  float(line[r_lat[0]:r_lat[1]])
+    lon  = -float(line[r_lon[0]:r_lon[1]])
+    return stn, lat, lon
+
+if __name__=='__main__':
+    args = get_command_line_args()
+    text = grab_website_text()
+    data = extract_illinois_section(text)
+    for line in data.splitlines():
         try:
-            lat  =  float(line[r_lat[0]:r_lat[1]])
-            lon  = -float(line[r_lon[0]:r_lon[1]])
+            stn, lat, lon = parse_station_line(line)
+            args.outfile.write("%s\t%+6.2f\t%+6.2f\n"%(stn, lon, lat))
         except ValueError:
-            # skip rows with invalid data
-            continue
-        args.outfile.write("%s\t%+6.2f\t%+6.2f\n"%(stn, lon, lat))
+            logging.warning('Could not parse line: {0}'.format(line))
